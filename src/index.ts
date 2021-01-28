06d3052b7e22d82aa4d9e62d0399509b3f43f58d
wYuanHpp <wangyuan85@gmail.com>
2021-01-28 18:28:33 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+/*
+{
+  isWorkSpaceEmpty:true,
+  getLastReleaseVersion: true, 
+  getLastPreReleaseVersion: true,  
+  preRelease: true,
+  release: true,
+}
+*/
+
+interface PublishHelperOption {
+
+}
+
+
+class PublishHelper {
+  constructor(option: PublishHelperOption) {
+
+  }
+
+  isWorkSpaceEmpty() {
+
+  } 
+
+  getLastReleaseVersion() {
+
+  }
+
+  getLastPreReleaseVersion() {
+
+  }
+
+  preRelease() {
+
+  }
+
+  release() {
+
+  }
+}
